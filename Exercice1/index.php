5198bf6f7fa61b21c87a8656603d185f85f77252
Diallo boubacar <diallob456@gmail.com>
2023-02-05 23:24:03 +0100

    Exercices_XP_GOLD

---
+++ Exercice1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+ <?php 
+ $total=1;
+  for ($i=1; $i <=10 ; $i++) { 
+      $total*=$i;
+  }
+   echo $total;
+  ?>
+</body>
+</html>
---
+++ Exercice2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+ 
+<?php 
+for ($i=1; $i <=8; $i++) { 
+
+	for ($j=0; $j <$i ; $j++) {
+	echo"*" ;
+		
+	}
+	echo "<br>";
+}
+
+ ?>
+
+</body>
+</html>
